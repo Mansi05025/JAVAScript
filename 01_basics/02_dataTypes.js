566ba2276897a8ba782466091bf1d2db5a27e020
Mansi05025 <125210271+Mansi05025@users.noreply.github.com>
2024-09-01 17:42:11 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,50 @@
+"use strict";  //treat all Js code as newer version
+
+//alert(3 + 3)  // we are using node.js , not browser
+
+console.log(3 + 3 )   // code  readability  should be high
+
+console.log("hey")
+
+let  name = "mahii"  // string
+let age = 18         // number
+let isLoggedIn = false  // boolean
+let state;
+
+
+
+   // ##  primitive
+// number =>  2 to power 53
+// bigint 
+// string  => "" , ''
+// boolean  =>  true / false
+// null  =>  standalone  value
+// undefined =>  value are not assign
+// symbol  =>  unique
+
+
+
+//  ## Object
+
+console.log(typeof null);      // null are object
+
+console.log(typeof null);      // object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
